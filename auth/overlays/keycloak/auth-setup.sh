42453f208010487d2c9b4379546afc2f33b4c397
DavidSpek <vanderspek.david@gmail.com>
2021-05-24 16:47:42 +0200

    Add keycloak authentication

---
+++ auth/overlays/keycloak/auth-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+COOKIE_SECRET=$(python3 -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(16)).decode())')
+OIDC_CLIENT_ID="oauth2-proxy"
+OIDC_CLIENT_SECRET="129789cf-c0b6-466b-87fa-512a60b2fd9a"
+
+kubectl create secret generic -n auth oauth2-proxy --from-literal=client-id=${OIDC_CLIENT_ID} --from-literal=client-secret=${OIDC_CLIENT_SECRET} --from-literal=cookie-secret=${COOKIE_SECRET} --dry-run=client -o yaml | kubeseal | yq eval -P > oauth2-proxy-secret.yaml
+
+DATABASE_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+read -p 'Keycloak Admin Password (not for Kubeflow sign in): ' KEYCLOAK_ADMIN_PASS
+read -p 'Keycloak Management Password (not for Kubeflow sign in): ' KEYCLOAK_MANAGEMENT_PASS
+
+kubectl create secret generic -n auth keycloak-secret --from-literal=admin-password=${KEYCLOAK_ADMIN_PASS} --from-literal=database-password=${DATABASE_PASS} --from-literal=management-password=${KEYCLOAK_MANAGEMENT_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > keycloak-secret.yaml
+
+read -p 'Email (for Kubeflow login): ' EMAIL
+read -p 'Username (for Kubeflow login): ' USERNAME
+read -p 'Password (for Kubeflow login): ' ADMIN_PASS
+
+yq eval -j -P ".users[0].username = \"${USERNAME}\" | .users[0].email = \"${EMAIL}\" | .users[0].credentials[0].value = \"${ADMIN_PASS}\" | .clients[0].clientId = \"${OIDC_CLIENT_ID}\" | .clients[0].secret = \"${OIDC_CLIENT_SECRET}\"" kubeflow-realm-template.json | kubectl create secret generic -n auth kubeflow-realm --dry-run=client --from-file=kubeflow-realm.json=/dev/stdin -o json | kubeseal | yq eval -P > kubeflow-realm-secret.yaml
