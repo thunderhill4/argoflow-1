76c07e9b4c215e852858daac3bef3aba56ced787
DavidSpek <vanderspek.david@gmail.com>
2021-05-24 16:57:34 +0200

    Fix auth setup OIDC credential generation

--- auth/overlays/keycloak/auth-setup.sh
+++ auth/overlays/keycloak/auth-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 COOKIE_SECRET=$(python3 -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(16)).decode())')
-OIDC_CLIENT_ID="oauth2-proxy"
-OIDC_CLIENT_SECRET="129789cf-c0b6-466b-87fa-512a60b2fd9a"
+OIDC_CLIENT_ID=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+OIDC_CLIENT_SECRET=$(python3 -c 'import secrets; print(secrets.token_hex(32))')
 
 kubectl create secret generic -n auth oauth2-proxy --from-literal=client-id=${OIDC_CLIENT_ID} --from-literal=client-secret=${OIDC_CLIENT_SECRET} --from-literal=cookie-secret=${COOKIE_SECRET} --dry-run=client -o yaml | kubeseal | yq eval -P > oauth2-proxy-secret.yaml
 
