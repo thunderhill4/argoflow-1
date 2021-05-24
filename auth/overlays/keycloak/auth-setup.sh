af99dfd4b5ab5920a7d3a6fbfb53f2ce1d28abc0
DavidSpek <vanderspek.david@gmail.com>
2021-05-24 17:08:05 +0200

    Fix Keycloak postgresql

--- auth/overlays/keycloak/auth-setup.sh
+++ auth/overlays/keycloak/auth-setup.sh
@@ -7,10 +7,15 @@ OIDC_CLIENT_SECRET=$(python3 -c 'import secrets; print(secrets.token_hex(32))')
 kubectl create secret generic -n auth oauth2-proxy --from-literal=client-id=${OIDC_CLIENT_ID} --from-literal=client-secret=${OIDC_CLIENT_SECRET} --from-literal=cookie-secret=${COOKIE_SECRET} --dry-run=client -o yaml | kubeseal | yq eval -P > oauth2-proxy-secret.yaml
 
 DATABASE_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+POSTGRESQL_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
 read -p 'Keycloak Admin Password (not for Kubeflow sign in): ' KEYCLOAK_ADMIN_PASS
 read -p 'Keycloak Management Password (not for Kubeflow sign in): ' KEYCLOAK_MANAGEMENT_PASS
 
 kubectl create secret generic -n auth keycloak-secret --from-literal=admin-password=${KEYCLOAK_ADMIN_PASS} --from-literal=database-password=${DATABASE_PASS} --from-literal=management-password=${KEYCLOAK_MANAGEMENT_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > keycloak-secret.yaml
+
+
+
+kubectl create secret generic -n auth keycloak-postgresql --from-literal=postgresql-password=${DATABASE_PASS} --from-literal=postgresql-postgres-password=${POSTGRESQL_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > postgresql-secret.yaml
 
 read -p 'Email (for Kubeflow login): ' EMAIL
 read -p 'Username (for Kubeflow login): ' USERNAME
